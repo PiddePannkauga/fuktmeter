f50a4764fcf7606124d449fcafb6abb9d5b325b0
PiddePannkauga <petter.mansson1@gmail.com>
2018-10-01 21:39:38 +0200

    server change to test another python script method

--- server/fuktserver.js
+++ server/fuktserver.js
@@ -2,22 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let runPy = new Promise(function(success, nosuccess) {
+
+  const { spawn } = require('child_process');
+  const pyprog = spawn('python', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
+
+  pyprog.stdout.on('data', function(data) {
+
+      success(data);
+  });
+
+  pyprog.stderr.on('data', (data) => {
+
+      nosuccess(data);
+  });
+});
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.get('/', (req, res) =>{
-  const {spawn} = require('child_process')
-  const pyProg = spawn('python', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
-  pyProg.stdout.on('data', function(data){
+  res.write('welcome\n');
 
-    console.log(data.toString());
-    res.write(data);
-    res.end('end')
+    runPy.then(function(fromRunpy) {
+        console.log(fromRunpy.toString());
+        res.end(fromRunpy);
+    });
   })
-   res.send(test('fuktet'))});
-
+    
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // const SerialPort = require('serialport')
