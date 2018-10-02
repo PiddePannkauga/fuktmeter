930a79da0318688e364cb7b525dc6088e4bcbfcf
petter.mansson1@gmail.com <petter.mansson1@gmail.com>
2018-10-02 22:05:44 +0200

    Fix child_process multiple spawn bug

--- server/fuktserver.js
+++ server/fuktserver.js
@@ -2,45 +2,47 @@ const express = require('express')
 const app = express()
 const port = 3200
 
-app.use(function(req, res,next) {
+app.use(function (req, res, next) {
   res.append("Access-Control-Allow-Origin", "*");
   res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 
+const {
+  spawn
+} = require('child_process');
+const pyprog = spawn('python3', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
+let runPy = new Promise((resolve, reject) => {
 
+  pyprog.stdout.on('data', function (data) {
+    console.log("Inside Resolve")
+    resolve(data);
 
+  });
 
+  pyprog.stderr.on('data', (data) => {
 
-app.get('/', (req, res) =>{
-  const { spawn } = require('child_process');
-  const pyprog = spawn('python3', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
-  let runPy = new Promise((resolve, reject) => {
-
-    pyprog.stdout.on('data', function(data) {
-        console.log("Inside Resolve")
-        resolve(data);
-        
-    });
-  
-    pyprog.stderr.on('data', (data) => {
-  
-        reject(data,'NoWork');
-    });
-
-  })
-
-    runPy.then(function(fromRunpy) {
-        console.log(fromRunpy.toString());
-        res.send(fromRunpy);
-        pyprog.kill('SIGINT')
-    }).catch((err) => {
-      console.log(err.toString())
-    });
+    reject(data, 'NoWork');
+  });
 
 })
-    
+
+
+
+app.get('/', (req, res) => {
+
+
+  runPy.then(function (fromRunpy) {
+    console.log(fromRunpy.toString());
+    res.send(fromRunpy);
+    pyprog.kill('SIGINT')
+  }).catch((err) => {
+    console.log(err.toString())
+  });
+
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // const SerialPort = require('serialport')
