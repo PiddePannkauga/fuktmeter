675c9899ffed8aaaafcfa258fda2df01ffab2b20
petter.mansson1@gmail.com <petter.mansson1@gmail.com>
2018-10-25 15:15:12 +0200

    Possible fix for issue with pythonscript output

--- server/python/Adafruit_Python_DHT/examples/simpletest.py
+++ server/python/Adafruit_Python_DHT/examples/simpletest.py
@@ -45,6 +45,5 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # If this happens try again!
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-    sys.stdout.flush()
 else:
     print('Failed to get reading. Try again!')
