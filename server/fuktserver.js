5e327183053c2c5e51aa283588315b4c093e5ed8
petter.mansson1@gmail.com <petter.mansson1@gmail.com>
2018-10-25 17:00:21 +0200

    Possible fix for issue with pythonscript output

--- server/fuktserver.js
+++ server/fuktserver.js
@@ -9,25 +9,28 @@ app.use(function (req, res, next) {
 });
 
 
-const {spawn} = require('child_process');
-const pyprog = spawn('python3', ['-u','./python/Adafruit_Python_DHT/examples/simpletest.py']);
 
-function run(){
+
+function run() {
   let runPy = new Promise((resolve, reject) => {
+    const {
+      spawn
+    } = require('child_process');
+    const pyprog = spawn('python3', ['-u', './python/Adafruit_Python_DHT/examples/simpletest.py']);
 
     pyprog.stdout.on('data', function (data) {
       console.log(data.toString())
-     resolve(data);
+      resolve(data);
     });
-  
+
     pyprog.stderr.on('data', (data) => {
       reject(data, 'NoWork');
     });
-  
-    pyprog.on('close', (code) =>{
+
+    pyprog.on('close', (code) => {
       console.log(code)
     })
-  
+
   })
   return runPy;
 }
@@ -35,8 +38,6 @@ function run(){
 
 
 app.get('/', (req, res) => {
-
-
 
   run().then(function (fromRunpy) {
     console.log(fromRunpy.toString())
