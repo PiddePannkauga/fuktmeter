b336da5b10c214ba69d402dc231d3ff04766c625
petter.mansson1@gmail.com <petter.mansson1@gmail.com>
2018-10-25 13:56:30 +0200

    Testing execFile on Childprocess

--- server/fuktserver.js
+++ server/fuktserver.js
@@ -9,8 +9,8 @@ app.use(function (req, res, next) {
 });
 
 
-const {spawn} = require('child_process');
-const pyprog = spawn('python3', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
+const {execFile} = require('child_process');
+const pyprog = execFile('python3', ['./python/Adafruit_Python_DHT/examples/simpletest.py']);
 let runPy = new Promise((resolve, reject) => {
 
   pyprog.stdout.on('data', function (data) {
